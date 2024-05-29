c8f84466b2a1d8b58ac49ba07ecb53d0006e6cb4
duoertai <duoertai@gmail.com>
2024-05-29 00:50:39 -0700

    config

--- src/main/java/ordersim/Simulator.java
+++ src/main/java/ordersim/Simulator.java
@@ -2,7 +2,12 @@ package ordersim;
 
 public class Simulator {
     public static void main(String[] args) {
-        ShelvesManager shelvesManager = new ShelvesManager(10, 10, 10, 15);
+        ShelvesManager shelvesManager = new ShelvesManager(
+                Config.HOT_SHELF_CAPACITY,
+                Config.COLD_SHELF_CAPACITY,
+                Config.FROZEN_SHELF_CAPACITY,
+                Config.OVERFLOW_SHELF_CAPACITY
+        );
         CourierDispatcher courierDispatcher = new CourierDispatcher(shelvesManager);
         Kitchen kitchen = new Kitchen(shelvesManager);
         OrderGenerator orderGenerator = new OrderGenerator();
